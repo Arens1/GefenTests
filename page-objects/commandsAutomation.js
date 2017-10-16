77ae407f05ca14c0b8d8204a683e77f04726eeb5
borisman4 <borism@gefen.online>
2017-10-16 10:56:22 +0300

    Create new automation and edit it (without deletion)

--- page-objects/commandsAutomation.js
+++ page-objects/commandsAutomation.js
@@ -7,58 +7,66 @@ const automationsCommands = {
     validateForm(){
         return this.waitForElementVisible('@createNew', 3000)
     },
-    createNewAutomation(){
+    //page object pause func
+    pause: function(time) {
+        this.api.pause(time)
+        return this
+    },
+    //page object refresh func
+    refresh: function() {
+        this.api.refresh()
+        return this
+    },
+    //using keys func
+    keys: function(browser){
+        this.api.keys(browser)
+        return this
+    },
+
+    createNewAuto(){
         return this.waitForElementVisible('@createNew', 3000)
             .click('@createNew')
-            .waitForElementVisible('@createNew', 3000)
-            .click('@createNew')
-            .waitForElementVisible('@eventsSubMenu', 3000)
+    },
+    chooseEvent(event){
+        return this.waitForElementVisible('@eventsSubMenu', 3000)
             .click('@eventsSubMenu')
-            .waitForElementVisible('@eventsNewWebsiteVisit', 3000)
-            .click('@eventsNewWebsiteVisit')
-            .waitForElementVisible('@channelSubMenu', 3000)
+            .waitForElementVisible('@'+event, 3000)
+            .click('@'+event)
+    },
+    chooseChannel(channel){
+         return this.waitForElementVisible('@channelSubMenu', 3000)
             .click('@channelSubMenu')
-            .waitForElementVisible('@pushWeb', 3000)
-            .click('@pushWeb')
-            .waitForElementVisible('@titleAutomation', 3000)
-            .setValue('@titleAutomation', "Test")
-            .waitForElementVisible('@messageAutomation', 4500)
-            .setValue('@messageAutomation', "Test_Message")
+            .waitForElementVisible('@'+channel, 3000)
+            .click('@'+channel)
+    },
+    inputAndSave(title,message){
+        return this.waitForElementVisible('@titleAutomation', 3000).pause(1000)
+            .clearValue('@titleAutomation')
+            .setValue('@titleAutomation', title).pause(1000)
+            .waitForElementVisible('@messageAutomation', 3500)
+            .clearValue('@messageAutomation')
+            .setValue('@messageAutomation', message).pause(1000)
             .waitForElementVisible('@saveNewAutomation', 3000)
             .click('@saveNewAutomation')
             .waitForElementVisible('@saveNotification', 3000) //wait for the 'automation saved' notification
-            //.api.refresh() ---> after refresh, can't select elements. so disabled refresh for now
-            .waitForElementVisible('@chooseFirstAutomation', 3000)
-            .click('@chooseFirstAutomation')
-            //edit newly created automation
+    },
+    chooseCreatedAuto(){
+        return this.refresh().pause(3000)
             .waitForElementVisible('@chooseCreatedAutomation', 3000)
             .click('@chooseCreatedAutomation')
-            .waitForElementVisible('@eventsSubMenu', 3000)
-            .click('@eventsSubMenu')
-            .waitForElementVisible('@eventsNewMessage', 3000)
-            .click('@eventsNewMessage')
-            .waitForElementVisible('@channelSubMenu', 3000)
-            .click('@channelSubMenu')
-            .waitForElementVisible('@pushApp', 3000)
-            .click('@pushApp')
-            .waitForElementVisible('@onByDefaultTickBox', 3000)
+    },
+    tickBoxes(box){
+        return this.waitForElementVisible('@onByDefaultTickBox', 3000)
             .click('@onByDefaultTickBox')
-            .waitForElementVisible('@runWhenOnlineTickBox', 3000)
-            .click('@runWhenOnlineTickBox')
-            .waitForElementVisible('@titleAutomation', 3000)
-            .clearValue('@titleAutomation')
-            .setValue('@titleAutomation', "TestAltered")
-            .waitForElementVisible('@messageAutomation', 4500)
-            .clearValue('@messageAutomation')
-            .setValue('@messageAutomation', "Test_Message_Altered")
-            .waitForElementVisible('@saveNewAutomation', 3000)
-            .click('@saveNewAutomation')
-            //.waitForElementVisible('@saveNotification', 3000) // sometimes, it won't work
-            //delete created automation - there is a known bug with the delete button
-            // .waitForElementVisible('@optionMenu', 3000)
-            // .click('@optionMenu')
-            // .waitForElementVisible('@deleteAutomation', 10000)
-            // .click('@deleteAutomation')
+            .waitForElementVisible('@'+box, 3000)
+            .click('@'+box)
+    },
+    deleteCreatedAuto() {
+        return this.waitForElementVisible('@optionMenu', 3000)
+            .click('@optionMenu')
+            .keys(this.api.Keys.RIGHT_ARROW)
+            .pause(4000)
+            .keys(this.api.Keys.ENTER)
     }
 };
 
@@ -75,50 +83,179 @@ module.exports = {
         eventsSubMenu: {
             selector: 'md-select[placeholder="Events"]'
         },
-        eventsNewWebsiteVisit: {
+        //list of events
+        NEW_VISIT: {
             selector: 'md-option[value="NEW_VISIT"]'
         },
-        eventsNewMessage: {
+        NEW_MESSAGE: {
             selector: 'md-option[value="NEW_MESSAGE"]'
         },
+        AUDIT_APPROVAL: {
+            selector: 'md-option[value="AUDIT_APPROVAL"]'
+        },
+        AUDIT_REJECT: {
+            selector: 'md-option[value="AUDIT_REJECT"]'
+        },
+        AUDIT_REQUEST: {
+            selector: 'md-option[value="AUDIT_REQUEST"]'
+        },
+        CAMPAIGN_DELIVERED: {
+            selector: 'md-option[value="CAMPAIGN_DELIVERED"]'
+        },
+        CAMPAIGN_ITEM_BOUNCED: {
+            selector: 'md-option[value="CAMPAIGN_ITEM_BOUNCED"]'
+        },
+        CAMPAIGN_ITEM_COMPLAINT: {
+            selector: 'md-option[value="CAMPAIGN_ITEM_COMPLAINT"]'
+        },
+        CAMPAIGN_SEND_FAILED: {
+            selector: 'md-option[value="CAMPAIGN_SEND_FAILED"]'
+        },
+        CAMPAIGN_SENT: {
+            selector: 'md-option[value="CAMPAIGN_SENT"]'
+        },
+        CHANNEL_UNSUBSCRIBE: {
+            selector: 'md-option[value="CHANNEL_UNSUBSCRIBE"]'
+        },
+        EMAIL_OPEN: {
+            selector: 'md-option[value="EMAIL_OPEN"]'
+        },
+        EMAIL_PUSH_UPDATE: {
+            selector: 'md-option[value="EMAIL_PUSH_UPDATE"]'
+        },
+        EMAIL_VISIBLE_PARENT: {
+            selector: 'md-option[value="EMAIL_VISIBLE_PARENT"]'
+        },
+        EMAIL_VISIBLE_TEMPLATE: {
+            selector: 'md-option[value="EMAIL_VISIBLE_TEMPLATE"]'
+        },
+        FILE_UPLOAD_FAILED: {
+            selector: 'md-option[value="FILE_UPLOAD_FAILED"]'
+        },
+        FILE_UPLOAD_FINISHED: {
+            selector: 'md-option[value="FILE_UPLOAD_FINISHED"]'
+        },
+        FORGOT_PASSWORD: {
+            selector: 'md-option[value="FORGOT_PASSWORD"]'
+        },
+        INSUFFICIENT_SERVICE_PLAN_BALANCE: {
+            selector: 'md-option[value="INSUFFICIENT_SERVICE_PLAN_BALANCE"]'
+        },
+        KNOWN_VISIT: {
+            selector: 'md-option[value="KNOWN_VISIT"]'
+        },
+        LINK_CLICK: {
+            selector: 'md-option[value="LINK_CLICK"]'
+        },
+        NEW_BU_RATING: {
+            selector: 'md-option[value="NEW_BU_RATING"]'
+        },
+        NEW_LEAD: {
+            selector: 'md-option[value="NEW_LEAD"]'
+        },
+        NEW_RATING: {
+            selector: 'md-option[value="NEW_RATING"]'
+        },
+        NEW_SYSTEM_MESSAGE: {
+            selector: 'md-option[value="NEW_SYSTEM_MESSAGE"]'
+        },
+        OFFLINE_MESSAGE: {
+            selector: 'md-option[value="OFFLINE_MESSAGE"]'
+        },
+        ORDER_CREATION: {
+            selector: 'md-option[value="ORDER_CREATION"]'
+        },
+        ORDER_PAID: {
+            selector: 'md-option[value="ORDER_PAID"]'
+        },
+        ORDER_PAYMENT_FAILED: {
+            selector: 'md-option[value="ORDER_PAYMENT_FAILED"]'
+        },
+        REFERRED_VISIT: {
+            selector: 'md-option[value="REFERRED_VISIT"]'
+        },
+        SMS_PUSH_UPDATE: {
+            selector: 'md-option[value="SMS_PUSH_UPDATE"]'
+        },
+        SMS_VISIBLE_PARENT: {
+            selector: 'md-option[value="SMS_VISIBLE_PARENT"]'
+        },
+        SMS_VISIBLE_TEMPLATE: {
+            selector: 'md-option[value="SMS_VISIBLE_TEMPLATE"]'
+        },
+        THEME_PAGE_FORCE_CHANGE: {
+            selector: 'md-option[value="THEME_PAGE_FORCE_CHANGE"]'
+        },
+        THEME_PAGE_OFFER_CHANGE: {
+            selector: 'md-option[value="THEME_PAGE_OFFER_CHANGE"]'
+        },
+        VISIT_ENDED: {
+            selector: 'md-option[value="VISIT_ENDED"]'
+        },
+        WEBSITE_SESSION_PAGE: {
+            selector: 'md-option[value="WEBSITE_SESSION_PAGE"]'
+        },
+        //end of events list
         channelSubMenu: {
             selector: 'md-select[placeholder="Channel"]'
         },
-        pushWeb: {
+        //list of channels
+        PUSH_WEB: {
             selector: 'md-option[value="PUSH_WEB"]'
         },
-        pushApp: {
+        PUSH_APP: {
             selector: 'md-option[value="PUSH_APP"]'
         },
+        SMS: {
+            selector: 'md-option[value="SMS"]'
+        },
+        EMAIL: {
+            selector: 'md-option[value="EMAIL"]'
+        },
+        WEBHOOK: {
+            selector: 'md-option[value="WEBHOOK"]'
+        },
+        //end of channels list
         titleAutomation: {
             selector: 'input[ng-model="$ctrl.automation.subject"]'
         },
         saveNewAutomation: {
-            selector: 'span[translate="COMPONENTS.GF_BOTTOM_TOOLBAR.SAVE"]'
+            selector: 'button[class="md-accent md-button ng-scope btn-save"]'
         },
         saveNotification: {
-            selector: 'md-toast > div > span' //sometimes this works better: 'md-toast > div '
+            selector: 'md-toast > div > span'
         },
         messageAutomation: {
             selector: 'textarea[ng-model="$ctrl.automation.push_message"]'
         },
-        chooseFirstAutomation: {
-            selector: 'button[aria-label="New Lead    via Push Web"]'
-        },
         chooseCreatedAutomation: {
             selector: 'body > div.ng-scope > div > div > div > gf-tabs > div > div > automations > div > div > md-list > md-content > gf-automation-list-item:nth-child(53) > md-list-item > div > button'
         },
+        //tick-boxes
         onByDefaultTickBox: {
             selector: 'body > div.ng-scope > div > div > div > gf-tabs > div > div > automations > div > div > div > gf-automation-detail > md-content > div > div.automation-content.layout-column.flex > div.automation-attributes.ng-scope.layout-column > md-checkbox > div._md-container'
         },
-        runWhenOnlineTickBox: {
-            selector: '#radio_1209 > div._md-container'
+        BccSender: {
+            selector: 'md-checkbox[ng-model="$ctrl.automation.bcc"]'
         },
+        runAlways: {
+            selector: 'md-radio-button[value="always"]'
+        },
+        userOnline: {
+            selector: 'md-radio-button[value="userOnline"]'
+        },
+        userOffline: {
+            selector: 'md-radio-button[value="userOffline"]'
+        },
+        none: {
+            selector: 'md-radio-button[value="none"]'
+        },
+        //end of tick-boxes
         optionMenu: {
             selector: 'body > div.ng-scope > div > div > div > gf-tabs > div > div > automations > div > div > md-list > md-content > gf-automation-list-item:nth-child(53) > md-list-item > div > div._md-list-item-inner > div > div.header.flex-grow > gf-menu > md-menu > button'
         },
         deleteAutomation: {
-            selector: '#menu_container_1938 > md-menu-content > md-menu-item > button'
+            selector: 'button[class="md-button ng-scope" translate="COMPONENTS.GF_BOTTOM_TOOLBAR.DELETE"]'
         }
     },
 }
--- tests/automation-command-object.js
+++ tests/automation-command-object.js
@@ -6,20 +6,61 @@ const login = loginCommandObj['Login with correct username and password'];
 const dashboard = dashboardCommandObj['click on operation card'];
 const operation = operationCommandObj['click Automation'];
 
-
-
 module.exports = {
-    'Login'(browser) {
-        login(browser, 'borism', '12345678*');
-    },
-    'open Operation'(browser) {
-        dashboard(browser);
-    },
-    'click on Automation tab'(browser) {
-        operation(browser)
-    },
-    'create new automation'(browser){
+    'CreateEditDeleteAutomation'(browser,username,password,event,channel,title,message,editedEvent,editedChannel,tickbox,editedTitle,editedMessage) {
         const automation = browser.page.commandsAutomation();
-        automation.createNewAutomation();
+        //browser.start_session();
+        login(browser,username,password);
+        dashboard(browser);
+        operation(browser);
+        automation.createNewAuto()
+        .chooseEvent(event)
+        .chooseChannel(channel)
+        .inputAndSave(title,message)
+        .chooseCreatedAuto()
+        .chooseEvent(editedEvent)
+        .chooseChannel(editedChannel)
+        .tickBoxes(tickbox)
+        .inputAndSave(editedTitle,editedMessage)
+        .deleteCreatedAuto().pause(2000)
+        browser.end();
+    },
+        // 'Login'(browser) {
+        //     login(browser, 'borism', '12345678*');
+        // },
+        // 'open Operation'(browser) {
+        //     dashboard(browser);
+        // },
+        // 'click on Automation tab'(browser) {
+        //     operation(browser)
+        // },
+        // 'click create new'(browser){
+        //     const automation = browser.page.commandsAutomation();
+        //     automation.createNewAuto();
+        // },
+        // 'choose an event'(browser){
+        //     const automation = browser.page.commandsAutomation();
+        //     automation.chooseEvent('@NEW_SYSTEM_MESSAGE');
+        // },
+        // 'choose a channel'(browser){
+        //     const automation = browser.page.commandsAutomation();
+        //     automation.chooseChannel('@PUSH_WEB');
+        // },
+        // 'input title and message and save'(browser){
+        //     const automation = browser.page.commandsAutomation();
+        //     automation.inputAndSave("testTitle","testMessage");
+        // },
+        // 'edit newly created automation'(browser){
+        //     const automation = browser.page.commandsAutomation();
+        //     automation.chooseCreatedAuto();
+        //     automation.chooseEvent(event);
+        //     automation.chooseChannel('@PUSH_APP');
+        //     automation.tickBoxes('@userOffline');
+        //     automation.inputAndSave("testTitleAltered","testMessageAltered");
+        // },
+        // 'delete created automation' (browser) {
+        //     const automation = browser.page.commandsAutomation();
+        //     automation.deleteCreatedAuto();
+        // }
     }
-}
+
